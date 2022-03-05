45ad82f78ed5b12678b350cb580dcf02c5e7642a
Venkatesh Krishnamurthy <kvenkatesh.iitm@gmail.com>
2022-03-04 23:01:24 -0500

    Clean up

--- test.py
+++ test.py
@@ -18,26 +18,24 @@ real_Z = np.array(data['Re(Z)/Ohm'])
 imag_Z = -1*np.array(data['-Im(Z)/Ohm'])
 np.savetxt('data.csv', np.column_stack((freq, real_Z, imag_Z)), delimiter=',')
 
-# data.to_csv('data.csv', columns=['freq/Hz', 'Re(Z)/Ohm', '-Im(Z)/Ohm'], header=False, index=False)
-
-
+# Read from csv file and pre-process
 frequencies, Z = preprocessing.readCSV('data.csv')
 frequencies, Z = preprocessing.ignoreBelowX(frequencies, Z)
 
 
-# Define impedance model
+# Define circuit with iniital values
 circuit = 'R0-p(R1,C1)-p(R2,C2)'
-initial_guess = [100, 10, 10, .1, 100]
+initial_guess = [18, 500, 0.0001, 100, 0.001]
 circuit = CustomCircuit(circuit, initial_guess=initial_guess)
 
+# Fit to circuit
 circuit.fit(frequencies, Z)
 print(circuit)
-Z_fit = circuit.predict(frequencies)
 
+# Plot
 fig, ax = plt.subplots()
 plot_nyquist(ax, Z, fmt='o')
-plot_nyquist(ax, Z_fit, fmt='-')
-
+plot_nyquist(ax, circuit.predict(frequencies), fmt='-')
 plt.legend(['Data', 'Fit'])
-# plt.show()
 plt.savefig('plot.png')
+plt.show()
